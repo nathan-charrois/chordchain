f3e9c30be8fd58d47138524a7698d3bb93a3d745
Nathan Charrois <nathancharrois@gmail.com>
2025-09-26 16:02:04 -0700

    Info

--- app/components/Game/GameTarget.tsx
+++ app/components/Game/GameTarget.tsx
@@ -1,13 +1,15 @@
-import { Card, Text } from '@mantine/core'
+import { Card, Divider, Text } from '@mantine/core'
 
 import { useGame } from './hooks/useGame'
 
 export default function GameTarget() {
-  const { target, status } = useGame()
+  const { target, status, guesses, maxGuesses } = useGame()
 
   return (
     <Card mb="md" bg="gray.1">
       <Text>{`Game status: ${status}`}</Text>
+      <Text>{`Guess: ${guesses.length + 1} of ${maxGuesses}`}</Text>
+      <Divider my="sm" />
       <Text>{`Find the hidden calculation that equals ${target}`}</Text>
     </Card>
   )
